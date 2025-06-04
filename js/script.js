b067f895af73940c2546f99196abeed135afc314
vitaliibobkin <bobking.vitaly.1987@gmail.com>
2025-06-04 13:37:44 +0300

    created program for output data about user(age, city, sport)

--- js/script.js
+++ js/script.js
@@ -1,22 +1,95 @@
 "use strict";
 
-// get value from prompt
-const firstValue = +prompt("Please enter first value")
-const secondValue = +prompt("Please enter second value")
-const thirdValue = +prompt("Please enter third value")
-
-//calculate average number
-function average(firstValue, secondValue, thirdValue){
-    const values = [firstValue, secondValue, thirdValue];
-
-    if (!values.every(Number.isFinite)) {
-        alert("Please enter a valid numbers");
-        return;
+//get a number of years from the user
+function getBirthYear() {
+    const birthYear = +prompt("Enter your year of birth:");
+    if (birthYear === null || isNaN(birthYear)) {
+        alert("It's a pity that you did not want to enter your date of birth.");
+        return null;
     }
-
-    const avg = values.reduce((sum, value) => sum + value, 0) / values.length;
-    alert(`average number is: ${avg}`);
+    return birthYear;
 }
 
-//output value
-average(firstValue, secondValue, thirdValue);
+//get city from the user
+function getCity() {
+    const city = prompt("Which city do you live in?");
+    if (city === null || city === "") {
+        alert("It's a pity that you did not want to enter your city.");
+        return null;
+    }
+    return city.trim();
+}
+
+//get sport from the user
+function getSport() {
+    const sport = prompt("What is your favorite sport?");
+    if (sport === null || sport === "") {
+        alert("It's a pity that you did not want to enter your favorite sport.");
+        return null;
+    }
+    return sport.trim();
+}
+
+//get information about a city
+function getUserInfoAboutCity(age, city) {
+    let message;
+
+    switch (city.toUpperCase()) {
+        case "KYIV":
+            message = `You are approximately ${age} years old.\nYou live in the capital of Ukraine.`;
+            break;
+        case "WASHINGTON":
+            message = `You are approximately ${age} years old.\nYou live in the capital of the USA.`;
+            break;
+        case "LONDON":
+            message = `You are approximately ${age} years old.\nYou live in the capital of the United Kingdom.`;
+            break;
+        default:
+            message = `You are approximately ${age} years old.\nYou live in the city of ${city}.`;
+    }
+
+    return message;
+}
+
+//get information about a sport
+function getUserFavoriteSport(sport) {
+    let message;
+
+    switch (sport.toUpperCase()) {
+        case "FOOTBALL":
+            message = "Cool! Do you want to become Lionel Messi?";
+            break;
+        case "BASKETBALL":
+            message = "Cool! Do you want to become LeBron James?";
+            break;
+        case "TENNIS":
+            message = "Cool! Do you want to become Roger Federer?";
+            break;
+        default:
+            message = "You almost a champion!";
+    }
+
+    return message;
+}
+
+// output information about the user
+function userInfo() {
+    const birthYear = getBirthYear();
+    if (birthYear === null) return;
+
+    const city = getCity();
+    if (city === null) return;
+
+    const sport = getSport();
+    if (sport === null) return;
+
+    const cityMessage = getUserInfoAboutCity(birthYear, city);
+    const sportMessage = getUserFavoriteSport(sport);
+
+    alert(`${cityMessage}\n${sportMessage}`);
+}
+
+userInfo();
+
+
+
